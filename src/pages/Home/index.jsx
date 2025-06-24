e7bec2070ae5dd34b684cc83a7cea20b9846de06
Thiago Martins <thiagoroyal05@icloud.com>
2025-06-24 00:44:28 -0300

    componente input interface

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 
 function Home() {
   return (
-    <div className="flex justify-center items-center bg-white opacity-80 w-10/12 justify-center items-center m-auto mt-20 mb-6 p-8 rounded">
+    <div className="flex justify-center items-center bg-white opacity-85 w-10/12 justify-center items-center m-auto mt-20 mb-6 p-8 rounded ">
 
       <div className="flex justify-start items-center w-4/5 text-xl bg-[#000000] opacity-95 w-full py-1 px-4 gap-2 rounded-2xl	">
 
